3d2d684341acc79775b8577eb68aaff0f6a2787c
Michael Muniz <michael.muniz94@gmail.com>
2018-08-27 23:46:23 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,32 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+  var myArray = [1]
+  extraArray = ["foo", ...myArray]
+  return extraArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  var myArray = [1]
+  myArray.unshift('foo')
+  return myArray
+}
+
+function addElementToEndOfArray(array, element) {
+  var myArray = [1]
+  extraArray = [...myArray, 'foo']
+  return extraArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  var myArray = [1]
+  myArray.push('foo')
+  return myArray
+}
+
+function accessElementInArray(array, index) {
+  var myArray = [1,2,3]
+  return myArray[2]
+}
+
+function 
