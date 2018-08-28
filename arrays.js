0e56aff6a536d9b838e3b7eba138f7bd0a1c78cc
Michael Muniz <michael.muniz94@gmail.com>
2018-08-28 00:34:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  var myArray = [1]
+  myArray = [1]
   myArray.unshift('foo')
   return myArray
 }
