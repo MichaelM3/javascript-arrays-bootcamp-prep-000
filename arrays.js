54140e897b8c384ef2ca10aecaa36d966a163de0
Michael Muniz <michael.muniz94@gmail.com>
2018-08-28 01:17:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
-  var myArray = [1]
-  extraArray = ["foo", ...myArray]
+  extraArray = ["foo", ...array]
   return extraArray
 }
 
